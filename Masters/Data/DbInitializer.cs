b9935b1236f98cf663e89efe187831880cb6b4f2
Shivanibusireddy <S531494@nwmissouri.edu>
2019-03-02 23:04:16 -0600

    Added seed data for Requirements and DegreePlanTermRequirements

--- Masters/Data/DbInitializer.cs
+++ Masters/Data/DbInitializer.cs
@@ -36,7 +36,8 @@ namespace Masters.Data
                 context.SaveChanges();
 
             }
-            if (context.Requirements.Any())
+
+           if (context.Requirements.Any())
             {
                 Console.WriteLine("Requirements already exist");
             }
@@ -44,14 +45,45 @@ namespace Masters.Data
             {
                 var requirements = new Models.Requirement[]
                 {
+                    new Models.Requirement {RequirementID=460,RequirementAbbrev ="DB",RequirementName ="44-460 Database"},
+new Models.Requirement {RequirementID=356,RequirementAbbrev ="NF",RequirementName="44-356 Network Fundamemtals"},
+new Models.Requirement {RequirementID=542,RequirementAbbrev="OOP",RequirementName="44-542 OOP with Java"},
+new Models.Requirement {RequirementID=563,RequirementAbbrev="Web apps",RequirementName="44-563 Web apps"},
+new Models.Requirement {RequirementID=560,RequirementAbbrev="ADB",RequirementName="44-560 ADB"},
+new Models.Requirement {RequirementID=555,RequirementAbbrev="NS",RequirementName="44-555 Network Security"},
+new Models.Requirement {RequirementID=618,RequirementAbbrev="PM",RequirementName="44-618 PM"},
 
-           
+
+
                 };
                 Console.WriteLine($"Inserted {requirements.Length} new Requirements");
 
                 foreach (Models.Requirement r in requirements)
                 {
-                    context.Degrees.Add(r);
+                    context.Requirements.Add(r);
+                }
+                context.SaveChanges();
+
+            }
+
+            if (context.DegreePlanTermRequirements.Any())
+            {
+                Console.WriteLine("DegreePlanTermRequirements already exist");
+            }
+            else
+            {
+                var degreePlanTermRequirements = new Models.DegreePlanTermRequirement[]
+                {
+                    new Models.DegreePlanTermRequirement {DegreePlanTermRequirementID=1,DegreePlanID=20,TermID=1,RequirementID=460},
+                    new Models.DegreePlanTermRequirement {DegreePlanTermRequirementID=2,DegreePlanID=20,TermID=1,RequirementID=356}
+
+
+                };
+                Console.WriteLine($"Inserted {degreePlanTermRequirements.Length} new DegreePlanTermRequirement");
+
+                foreach (Models.DegreePlanTermRequirement dp in degreePlanTermRequirements)
+                {
+                    context.DegreePlanTermRequirements.Add(dp);
                 }
                 context.SaveChanges();
 
